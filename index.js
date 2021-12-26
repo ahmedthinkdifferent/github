f618dd707a6a9809425208fbc9f55cc9f1a649e2
ahmedthinkdifferent <ahmed.thinkdifferent@gmail.com>
2021-12-26 21:14:27 +0200

    add docker config

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-console.log(1);
-const name = "ahmed";
-console.log(name);
-const users = [{ name: "ahmed" }, {name:"gamal"}];
-console.log(users);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
