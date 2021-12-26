2ff7b9e1987e9236d580380c2810d923215f30c5
ahmedthinkdifferent <ahmed.thinkdifferent@gmail.com>
2021-12-26 23:46:09 +0200

    user api

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/users', (req, res) => {
+  res.send([{name:"ahmed"}, {name:"hassan"}]);
+})
+
+
+app.get('/users/1', (req, res) => {
+  res.send({name:"hassan"});
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/users', (req, res) => {
-  res.send([{name:"ahmed"}, {name:"hassan"}]);
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
