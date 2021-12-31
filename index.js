b599c9b930faf97ea244a3e3e1bd7f285ac30014
ahmedthinkdifferent <ahmed.thinkdifferent@gmail.com>
2021-12-31 18:43:41 +0200

    secret config

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let fs = require('fs');
 
 app.get('/users', (req, res) => {
-  res.send([{name:"ahmed"}, {name:"hassan"}]);
+    res.send([{name: "ahmed"}, {name: "hassan"}, {name: "mohamed"}, {name: "gamal"}]);
 })
 
 
 app.get('/users/1', (req, res) => {
-  res.send({name:"hassan"});
+    res.send({name: "hassan"});
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+});
+
+app.get('/readConfig', (req, res) => {
+    let content = fs.readFileSync("/config1").toString()
+    res.send(content)
+});
+
+app.get('/readSecret', (req, res) => {
+    let content = fs.readFileSync("/run/secrets/secret1").toString()
+    res.send(content)
 });
 
 
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 });
